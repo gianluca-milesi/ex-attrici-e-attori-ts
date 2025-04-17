7f0e09bd36b2ff005f9ed47af432767d583fc4ee
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-04-17 13:08:48 +0200

    ADD: Milestone 1 and 2. Type Person and Actress

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,27 @@
-console.log("ciao")
+type Person = {
+  readonly id: number,
+  readonly name: string,
+  birth_year: number,
+  death_year?: number,
+  biography: string,
+  image: string
+}
+
+type ActressNationality =
+  | "American"
+  | "British"
+  | "Australian"
+  | "Israeli-American"
+  | "South African"
+  | "French"
+  | "Indian"
+  | "Israeli"
+  | "Spanish"
+  | "South Korean"
+  | "Chinese"
+
+type Actress = Person & {
+  most_famous_movies: [string, string, string],
+  awards: string,
+  nationality: ActressNationality
+}
